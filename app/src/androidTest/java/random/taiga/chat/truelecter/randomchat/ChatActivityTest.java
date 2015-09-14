cebe8056f99a3540708a9aed4af20f2c03dfca5e
_TrueLecter_ <andrew.panassiouk@gmail.com>
2015-09-14 16:14:51 +0300

    Initial release

---
+++ app/src/androidTest/java/random/taiga/chat/truelecter/randomchat/ChatActivityTest.java
@@ -0,0 +1,18 @@
+package random.taiga.chat.truelecter.randomchat;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by _TrueLecter_ on 12.09.2015 for RandomCraft
+ * Licensed under terms of GPLv3 http://www.gnu.org/licenses/gpl.html
+ */
+public class ChatActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnDestroy() throws Exception {
+
+    }
+}
